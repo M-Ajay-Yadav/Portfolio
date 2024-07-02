98673b656f33e02ef8a92760c67892071103b927
M-Ajay-Yadav <majay805@gmail.com>
2024-07-02 22:15:42 +0530

    added db and removed some files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDb = require("./utils/db");
+
 const bodyParser = require('body-parser');
+
 const educationRoutes = require('./Routes/education');
 const skillRoutes = require('./Routes/skill');
 const projectRoutes = require('./Routes/project');
@@ -10,22 +14,28 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/portfolio');
+// mongoose.connect('mongodb://localhost:27017/portfolio');
 
-const db = mongoose.connection;
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// const db = mongoose.connection;
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
 
-const personalInfoRouter = require('./routes/personalInfo');
-app.use('/api/personalInfo', personalInfoRouter);
+// const personalInfoRouter = require('./routes/personalInfo');
+// app.use('/api/personalInfo', personalInfoRouter);
 
 app.use('/api/education', educationRoutes);
 app.use('/api/skills', skillRoutes);
 app.use('/api/projects', projectRoutes);
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = 5000;
+try {
+    connectDb().then(()=>{
+        app.listen(PORT,() => {
+            console.log(`server is running at :${PORT}`);
+        });
+    });
+} catch (error) {
+    console.log("server",error);
+}
