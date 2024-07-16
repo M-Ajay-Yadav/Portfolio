a7623992296f0f53ef8367e100d3d177cbd0f2bb
M-Ajay-Yadav <majay805@gmail.com>
2024-07-16 17:54:02 +0530

    server changes made in the index.js to deploy the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,37 +1,41 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const connectDb = require("../utils/db");
-const bodyParser = require('body-parser');
-const url = require('url');
-const educationRoutes = require('../Routes/education');
-const skillRoutes = require('../Routes/skill');
-const projectRoutes = require('../Routes/project');
+const bodyParser = require("body-parser");
+const url = require("url");
+const educationRoutes = require("../Routes/education");
+const skillRoutes = require("../Routes/skill");
+const projectRoutes = require("../Routes/project");
 
-
-const BASE_URL = process.env.BASE_URL;
-const parsedUrl = url.parse(BASE_URL);
-const basePath = parsedUrl.path;
+const BASE_URL = process.env.RENDER_EXTERNAL_URL;
+// const BASE_URL =process.env.BASE_URL;
+if (!BASE_URL) {
+  throw new Error("BASE_URL is not defined");
+}
+// const parsedUrl = url.parse(BASE_URL);
+// const basePath = parsedUrl.path || "";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.use(`${basePath}education`, educationRoutes);
-app.use(`${basePath}skills`, skillRoutes);
-app.use(`${basePath}projects`, projectRoutes);
-console.log(`${basePath}`)
-
+app.use("/education", educationRoutes);
+app.use("/skills", skillRoutes);
+app.use("/projects", projectRoutes);
+// console.log(`${basePath} `);
+//  app.use(`${basePath}education`, educationRoutes);
+// app.use(`${basePath}skills`, skillRoutes);
+// app.use(`${basePath}projects`, projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 try {
-    connectDb().then(()=>{
-        app.listen(PORT,() => {
-            console.log(`server is running at :${PORT} `);
-        });
+  connectDb().then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at :${PORT} `);
     });
+  });
 } catch (error) {
-    console.log("server",error);
+  console.log("server", error);
 }
