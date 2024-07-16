c015a964a545bd61093772612ca167cd4d4f4219
M-Ajay-Yadav <153363272+M-Ajay-Yadav@users.noreply.github.com>
2024-07-16 16:36:17 +0530

    Delete server directory

--- server/Models/Project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProjectSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  link: String
-});
-
-module.exports = mongoose.model('Project', ProjectSchema);
--- server/Models/Skill.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SkillSchema = new mongoose.Schema({
-  name: String,
-  proficiency: String
-});
-
-module.exports = mongoose.model('Skill', SkillSchema);
